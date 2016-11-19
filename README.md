b96dfbdc145ed87d791ffd72f11a5374d0c43939
Niklas Claesson <nicke.claesson@gmail.com>
2016-11-19 16:27:46 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # EPM
 
 EPM helps you compile and maintain your epics installation.
+
+# Install
+
+```bash
+pip install git+https://github.com/NickeZ/epm.git
+```
+
+# Usage
+
+```bash
+epm --help
+```
--- epm/epm.py
+++ epm/epm.py
@@ -1,2 +1,25 @@
+"""EPM"""
+from __future__ import print_function
+
+import argparse
 def main(args):
-    print('Hello World!')
+    """Main function"""
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='commands', dest='command')
+    parser_new = subparsers.add_parser('new', help='Create a new project')
+    parser_build = subparsers.add_parser('build', help='Compile project')
+    parser_clean = subparsers.add_parser('clean', help='Delete compiled files')
+
+    parser_new.add_argument('project-name')
+
+    args = parser.parse_args(args)
+
+    if args.command == 'new':
+        print('new')
+    elif args.command == 'build':
+        print('build')
+    elif args.command == 'clean':
+        print('clean')
+
+    return 0
