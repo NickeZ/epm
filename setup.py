add82c5d5a978f10e64772523a3a5e4bb25b9b51
Niklas Claesson <nicke.claesson@gmail.com>
2016-11-20 14:28:10 +0100

    wip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epm',
-    version='0.0.1',
+    version='0.1.0',
     description='EPM',
     long_description='EPM',
     url='https://github.com/NickeZ/epm',
