361c363b3ef46b611b50c40ec1df83cdfb079886
Niklas Claesson <nicke.claesson@gmail.com>
2016-11-29 21:33:57 +0100

    wip

--- tools/generate_linux_host_arch.py
+++ tools/generate_linux_host_arch.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Helper program to generate os specific epics host architectures"""
 import argparse
+import os
 import sys
 import platform
+import subprocess
+import tempfile
 from string import Template
 import pkg_resources
 
@@ -21,6 +24,8 @@ CONFIG_FILES_HOST = CONFIG_FILES + [
 CONFIG_FILES_CROSS = [
     'CONFIG.{0}.{1}']
 
+OSDIR = os.path.join('configure', 'os')
+
 def main():
     """Main function"""
     parser = argparse.ArgumentParser()
@@ -29,25 +34,36 @@ def main():
 
     generic_epics_arch = '{}-{}'.format(platform.system().lower(), platform.machine())
 
+    tmpdir = tempfile.mkdtemp()
+
+    osdir = os.path.join(tmpdir, OSDIR)
+
+    if not os.path.isdir(osdir):
+        os.makedirs(osdir)
+
     generate_files(
+        osdir,
         args.os,
         generic_epics_arch,
         CONFIG_FILES_HOST
     )
     generate_files(
+        osdir,
         '{}-debug'.format(args.os),
         '{}-debug'.format(generic_epics_arch),
         CONFIG_FILES_DEBUG
     )
     generate_files_cross(
+        osdir,
         args.os,
         '{}-debug'.format(args.os),
         generic_epics_arch,
         '{}-debug'.format(generic_epics_arch),
         CONFIG_FILES_CROSS
     )
+    subprocess.call('tar cf {}.tar.gz configure -C {}'.format(args.os, tmpdir), shell=True)
 
-def generate_files(epics_arch, generic_epics_arch, config_files):
+def generate_files(osdir, epics_arch, generic_epics_arch, config_files):
     """Generate epics_arch with includes of generic_epics_arch"""
     for config in config_files:
         tmpl_filename = config.format('linuxos')
@@ -56,7 +72,7 @@ def generate_files(epics_arch, generic_epics_arch, config_files):
             'resources/{}'.format(tmpl_filename)
         ).decode('utf-8')
 
-        config_filename = config.format(epics_arch)
+        config_filename = os.path.join(osdir, config.format(epics_arch))
         with open(config_filename, 'wb') as outfile:
             outfile.write(Template(tmpl_in).substitute(
                 linuxos=epics_arch,
@@ -64,7 +80,7 @@ def generate_files(epics_arch, generic_epics_arch, config_files):
                 generictarget=generic_epics_arch,
                 ).encode('utf-8'))
 
-def generate_files_cross(epics_arch, target_arch, generic_epics_arch, generic_target_arch,
+def generate_files_cross(osdir, epics_arch, target_arch, generic_epics_arch, generic_target_arch,
                          config_files):
     """Generate epics_arch with includes of generic_epics_arch"""
     for config in config_files:
@@ -74,7 +90,7 @@ def generate_files_cross(epics_arch, target_arch, generic_epics_arch, generic_ta
             'resources/{}'.format(tmpl_filename)
         ).decode('utf-8')
 
-        config_filename = config.format(epics_arch, target_arch)
+        config_filename = os.path.join(osdir, config.format(epics_arch, target_arch))
         with open(config_filename, 'wb') as outfile:
             outfile.write(Template(tmpl_in).substitute(
                 linuxos=epics_arch,
