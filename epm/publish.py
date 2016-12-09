e95893809bf994ebcfaa74574f2fe26186b986ff
Niklas Claesson <nicke.claesson@gmail.com>
2016-12-09 10:33:19 +0100

    wip

--- epm/publish.py
+++ epm/publish.py
@@ -2,10 +2,11 @@
 import tempfile
 import subprocess
 import os
+import shutil
 import siphash
 import requests
 
-from .util import pretty_print, load_project_config
+from .util import pretty_print, pretty_eprint, load_project_config
 from .constants import SIPHASH_KEY, EPM_SERVER
 
 def publish(path):
@@ -35,6 +36,14 @@ def publish(path):
         chksum = siphash.SipHash_2_4(SIPHASH_KEY, archive.read()).hexdigest()
 
     files = {'project': open(archivename, 'rb')}
-    requests.put('{}/api/v1/packages/new'.format(EPM_SERVER), files=files)
+    response = requests.put('{}/api/v1/packages/new'.format(EPM_SERVER), files=files)
     print('Filename: {}'.format(archivename))
     print('Checksum: {}'.format(chksum))
+    if response.status_code == 200:
+        r = response.json()
+        if 'data' in r and 'chksum' in r['data'] and r['data']['chksum'].encode('utf-8') == chksum:
+            pretty_print('Uploaded', 'Finished uploading {}'.format(name))
+            shutil.rmtree(tmpdir)
+            return
+
+    pretty_eprint('Error', 'Failed to upload')
